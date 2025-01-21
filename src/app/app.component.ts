aa695fb4759e96cca65dec8dcf22b693d3542cb9
Sinedin <sinedindnd@hotmail.com>
2025-01-21 16:26:58 -0500

    Creacion componente navbar usando flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './views/shared/header/header.component';
+import { NavbarComponent } from './views/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
